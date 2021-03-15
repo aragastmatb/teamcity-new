585f0439854cc7a83d4f0b3cdbdff8f557164b5e
admin <admin@TeamCity>
2021-03-15 17:31:59 +0000

    TeamCity change in 'Teamcity New' project: general settings of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -35,7 +35,7 @@ project {
 object Build : BuildType({
     name = "Build"
 
-    artifactRules = "/target/*.jar"
+    artifactRules = "target/*.jar"
 
     vcs {
         root(DslContext.settingsRoot)
